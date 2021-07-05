4d02f3df47b13806b164e63b64023a01d4a85d81
Bisheshxx <bishesh.tuladhar1@gmail.com>
2021-07-05 22:37:17 +0545

    react dom

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter,Switch, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+      
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
